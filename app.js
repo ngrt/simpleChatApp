afe6659eda9a0490c60bcdff823e0b9c0a4c516e
Noufel Gouirhate <noufel.gouirhate@gmail.com>
2017-12-24 19:56:43 +0100

    client and server sides ok

--- app.js
+++ app.js
@@ -11,11 +11,12 @@ app.use(express.static('public'))
 
 //routes
 app.get('/', (req, res) => {
-	res.send('Hello world')
+	res.render('index')
 })
 
 //Listen on port 3000
 server = app.listen(3000)
+
 
 
 //socket.io instantiation
@@ -24,5 +25,19 @@ const io = require("socket.io")(server)
 
 //listen on every connection
 io.on('connection', (socket) => {
+	console.log('New user connected')
 
+	//default username
+	socket.username = "Anonymous"
+
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    })
+
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    })
 })
---
+++ public/chat.js
@@ -0,0 +1,31 @@
+
+$(function(){
+   	//make connection
+	var socket = io.connect('http://localhost:3000')
+
+	//buttons and inputs
+	var message = $("#message")
+	var username = $("#username")
+	var send_message = $("#send_message")
+	var send_username = $("#send_username")
+	var chatroom = $("#chatroom")
+
+	//Emit message
+	send_message.click(function(){
+		socket.emit('new_message', {message : message.val()})
+	})
+
+	//Listen on new_message
+	socket.on("new_message", (data) => {
+		console.log(data)
+		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+	})
+
+	//Emit a username
+	send_username.click(function(){
+		console.log(username.val())
+		socket.emit('change_username', {username : username.val()})
+	})
+});
+
+
